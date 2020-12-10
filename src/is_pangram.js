c61c69b4a01c94c8a12e7f70223546f8849c26ad
Stacy <lundquist.stacy@gmail.com>
2020-12-09 23:07:21 -0800

    small change, 3 passing

--- src/is_pangram.js
+++ src/is_pangram.js
@@ -1,5 +1,6 @@
 const isPangram = function(text) {
-    const letters = text.toLowercase().match(/[a-z]/g);
+    const input = text.toLowerCase()
+    const letters = input.match(/[a-z]/g);
     const letterCount = new Set(letters);
     if (letterCount.length === 26) {
         return true;
